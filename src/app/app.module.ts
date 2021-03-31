72c0e54e8d039fd94b64931a69cf95058a0595ca
Erick Chan <47164956+erickcu1416@users.noreply.github.com>
2021-03-31 13:31:56 -0600

    inital

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { OrigamiFormsModule } from '@codebakery/origami/forms';
+
 import { AppComponent } from './app.component';
+import "@ui5/webcomponents/dist/Button";
+import "@ui5/webcomponents-fiori/dist/Bar.js";
+import "@ui5/webcomponents-icons/dist/Assets.js";
+import { HeaderComponent } from './components/header/header.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {NgbCarouselModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    OrigamiFormsModule,
+    NgbModule,
+    NgbCarouselModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ]
 })
 export class AppModule { }
